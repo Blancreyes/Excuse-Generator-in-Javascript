6172f72c4e182031e77601aac103bcdb07e0f201
Blancreyes <luijbr83@gmail.com>
2022-12-10 10:54:48 +0000

    Excuse Generator Created

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuseGenerator();
 };
+
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let i = Math.floor(Math.random() * who.length);
+  let j = Math.floor(Math.random() * action.length);
+  let k = Math.floor(Math.random() * what.length);
+  let l = Math.floor(Math.random() * when.length);
+
+  return who[i] + " " + action[j] + " " + what[k] + " " + when[l];
+}
+console.log(excuseGenerator);
